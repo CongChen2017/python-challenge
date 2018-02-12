11755380bf0b8b7f7a45075fd2a28fb5c7499118
congchen2017 <cong-chen@northwestern.edu>
2018-02-11 18:21:53 -0600

    finished code and test. For multiple paragraphs in one file, the code will combine them and count as one paragraph.

---
+++ PyParagraph/main.py
@@ -0,0 +1,79 @@
+# # get a paragraph from input and store as sting
+	
+# paragraph = input("please input your paragraph here: ")
+
+
+# read text files from a folder
+
+import os
+
+# specify folder name
+
+folderName = 'raw_data'
+
+# Loop through files in target folder
+
+fileNames = os.listdir(folderName)
+
+for filename in fileNames:
+
+	# set file path
+
+	textpath = os.path.join(folderName, filename)
+
+	# read text files
+
+	with open(textpath, newline='') as textfile:
+
+		paragraph = ''.join(textfile.readlines())
+
+	# print title of the count result
+
+	print("")
+	print("Paragraph Analysis")
+	print("-------------------")
+
+	# split paragraph as words
+
+	import re
+	word = re.split("\s|(?<!\d)[.,][,.](?!\d)", paragraph)
+
+
+	# print out word count
+	print("Approximate Word Count: " + str(len(word)))
+
+	# split paragraph as sentence
+
+	sentence = re.split(r'[.!?]+', paragraph)
+
+	# print out sentence count
+	print("Approximate Sentence Count: " + str(len(sentence)-1))
+
+	# generate list of alphabets
+
+	Alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 
+	            'g', 'h', 'i', 'j', 'k', 'l', 
+	            'm', 'n', 'o', 'p', 'q', 'r', 
+	            's', 't', 'u', 'v', 'w', 'x', 
+	            'y', 'z']
+
+	# initiate a list of letters
+
+	letter = []
+
+	# loop paragraph, find letters and add to list
+
+	for char in list(paragraph):
+
+		if char in Alphabet:
+
+			letter.append(char)
+
+	# print out average letter count / word
+	print("Average Letter Count: " + str("{0:.2f}".format(len(letter)/len(word))))
+
+	# print out Average sentence length
+	print("Average Sentence Length: " + str("{0:.1f}".format(len(word)/(len(sentence)-1))))
+	print("")
+
+
